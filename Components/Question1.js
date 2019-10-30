605fb3c947d54b9cb29754790618e82ccbeae034
Yousef Al Othman <Yousef@Yousef.local>
2019-10-30 14:34:41 +0300

    home, question almost done

--- Components/Question1.js
+++ Components/Question1.js
@@ -1,28 +1,65 @@
 import React, { Component } from "react";
-import { Container, Header, Content, Button, Text } from "native-base";
+import { Container, Header, Content, View, Button, Text } from "native-base";
 import { Col, Row, Grid } from "react-native-easy-grid";
+import { StyleSheet } from "react-native";
 
-export default class Question1 extends Component {
+export default class Home extends Component {
   render() {
     return (
       <Container>
-        <Header />
-        <Row>
-          <Grid>
-            <Col style={{ backgroundColor: "#BC0000", height: 50 }}></Col>
-            <Col style={{ backgroundColor: "#838BEB", height: 200 }}></Col>
-          </Grid>
-        </Row>
         <Grid>
-          <Content>
-            <Col style={{ backgroundColor: "#635DB7", height: 200 }}></Col>
-            <Col style={{ backgroundColor: "#00CE9F", height: 200 }}></Col>
-            {/* <Button large>
-              <Text>Start</Text>
-            </Button> */}
-          </Content>
+          <Row
+            style={{
+              // vertical
+              alignItems: "center",
+              // horizontal
+              alignSelf: "center"
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "Futura",
+                fontWeight: "800",
+                color: "#BC0000"
+              }}
+            >
+              Meal type?
+            </Text>
+          </Row>
+
+          <Row>
+            <Col>
+              <Button style={{ height: 100, weight: 100 }}>
+                <Text>Breakfast</Text>
+              </Button>
+              <Button style={{ height: 100, weight: 100 }}>
+                <Text>Lunch</Text>
+              </Button>
+            </Col>
+            <Col>
+              <Button style={{ height: 100, weight: 100 }}>
+                <Text>Dinner</Text>
+              </Button>
+              <Button style={{ height: 100, weight: 100 }}>
+                <Text>Dessert</Text>
+              </Button>
+            </Col>
+          </Row>
+
+          <Row></Row>
         </Grid>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  circle: {
+    width: 200,
+    height: 200,
+    borderRadius: 200 / 2,
+    backgroundColor: "#BC0000",
+    alignSelf: "center",
+    justifyContent: "center"
+  }
+});
--- Navigation/MyStackNav.js
+++ Navigation/MyStackNav.js
@@ -1,10 +1,12 @@
 import { createStackNavigator } from "react-navigation-stack";
 
 import Home from "../Components/Home";
+import Question1 from "../Components/Question1";
 
 const MyStackNav = createStackNavigator(
   {
-    HomeScreen: Home
+    HomeScreen: Home,
+    Question1Screen: Question1
   },
   {
     initialRouteName: "HomeScreen"
