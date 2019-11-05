18710007bb0deab149f5c7ded9bab06796682dee
NaserAldeen <30509@aum.edu.kw>
2019-11-05 11:05:37 +0300

    final decision

--- Components/FinalRecommendation.js
+++ Components/FinalRecommendation.js
@@ -1,23 +1,50 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Spinner } from "native-base";
 
 export class FinalRecommendation extends Component {
-  static propTypes = {
-    prop: PropTypes
+  state = {
+    restaurant: null
   };
 
   render() {
-    return (
-      <View>
-        <Text> Suck a dick bitch </Text>
-      </View>
-    );
+    this.props.socket.socket.on("give_result", data => {
+      this.setState({ restaurant: data });
+    });
+    console.log(this.state.restaurant);
+    if (!this.state.restaurant)
+      return (
+        <Spinner
+          color="red"
+          style={{
+            width: "30%",
+            height: "30%",
+            marginTop: "auto",
+            marginBottom: "auto",
+            alignSelf: "center"
+          }}
+        />
+      );
+    else
+      return (
+        <View>
+          <Image
+            style={{ height: 450, marginTop: 50 }}
+            source={{ uri: this.state.restaurant.image }}
+          ></Image>
+          <Text style={{ fontSize: 40, alignSelf: "center", marginTop: 20 }}>
+            {this.state.restaurant.name}{" "}
+          </Text>
+        </View>
+      );
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  socket: state.socket
+});
 
 const mapDispatchToProps = {};
 
